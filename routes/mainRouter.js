82f3917e6277e42d1b393038a82df214097373a0
Dev Samir Dalia <devdalia9@gmail.com>
2023-08-21 15:38:48 +0530

    In hurry, forgot to add the / path(to indicate that API is working) instead of you seeing  Cannot GET

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   return res.send(
-    "Checkout link: " + "<a href = 'https://documenter.getpostman.com/view/17874220/2s9Y5Ty4UF'>PostMan Documentation</a>"
+    "(API Working)Checkout link: " + "<a href = 'https://documenter.getpostman.com/view/17874220/2s9Y5Ty4UF'>PostMan Documentation</a>"
   );
 });
 
