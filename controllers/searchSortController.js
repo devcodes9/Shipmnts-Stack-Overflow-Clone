4709d4883f76b95f5966943cdaf9efb26c1844c4
Dev Samir Dalia <devdalia9@gmail.com>
2023-08-21 12:53:12 +0530

    Sorting Added

--- controllers/searchSortController.js
+++ controllers/searchSortController.js
@@ -2,7 +2,7 @@ const Question = require("../models/Question");
 
 const searchQuestions = async (req, res) => {
   try {
-    let { query } = req.query;
+    let { query, tag, order } = req.query;
 
     if (!query) {
       return res.status(400).json({
@@ -11,12 +11,39 @@ const searchQuestions = async (req, res) => {
       });
     }
 
+    let sortOptions = {};
+
+    if (!tag && !order) {
+      sortOptions = { votes: -1 };
+    } else {
+      if (tag == "time") {
+        tag = "createdAt";
+      } else if (tag == "vote") {
+        tag = "votes";
+      } else {
+        return res.status(400).json({
+          success: false,
+          message: "Please enter valid tag",
+        });
+      }
+      if (order === "asc") {
+        sortOptions = { [tag]: 1 };
+      } else if (order === "desc") {
+        sortOptions = { [tag]: -1 };
+      } else {
+        return res.status(400).json({
+          success: false,
+          message: "Please enter valid order",
+        });
+      }
+    }
+
     const searchResults = await Question.find({
       $or: [
         { title: { $regex: query, $options: "i" } },
         { description: { $regex: query, $options: "i" } },
       ],
-    });
+    }).sort(sortOptions);
 
     // const searchResultsFromAnswer = await searchAnswers(query);
 
